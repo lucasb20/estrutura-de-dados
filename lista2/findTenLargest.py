2760c8c0a1375b9f9c089f800dfd34e9ad56e760
Lucas Rocha <lr359508@gmail.com>
2024-03-03 14:22:30 -0300

    feat: findTenLargest created

---
+++ lista2/findTenLargest.py
@@ -0,0 +1,21 @@
+import random
+
+def findTenLargest(array):
+    maiores = []
+    array_copia = array.copy()
+
+    for _ in range(10):
+        i_max = 0
+        maior = array_copia[0]
+        for i, num in enumerate(array_copia):
+            if num > maior:
+                maior = num
+                i_max = i
+        maiores.append(maior)
+        array_copia.pop(i_max)
+
+    return maiores
+
+array = [random.randint(0,100) for _ in range(25)]
+print(array)
+print(findTenLargest(array))

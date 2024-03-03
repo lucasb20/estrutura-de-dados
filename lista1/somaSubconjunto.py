f765e419708d9aab5fa229a8b852d0e8c72cf97b
Lucas Rocha <lr359508@gmail.com>
2024-03-03 11:17:42 -0300

    fix: somaSubconjunto optimized

--- lista1/somaSubconjunto.py
+++ lista1/somaSubconjunto.py
@@ -1,8 +1,9 @@
-def somaSubconjunto(inicio, numeros, total, soma):
-    if inicio >= total:
+def somaSubconjunto(index, numeros, soma):
+    if index == len(numeros):
         return soma == 0
-    if somaSubconjunto(inicio+1, numeros, total, soma-numeros[inicio]):
+    if somaSubconjunto(index+1, numeros, soma-numeros[index]):
         return True
-    return somaSubconjunto(inicio+1, numeros, total, soma)
-    
-print(somaSubconjunto(0, [1,2,3,4], 4, 9))
+    return somaSubconjunto(index+1, numeros, soma)
+
+
+print(somaSubconjunto(0, [1,2,3,4], 9))

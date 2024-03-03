c182262116105ed7b552d3fcc51d0fe85d67ce8a
Lucas Rocha <lr359508@gmail.com>
2024-03-03 10:29:03 -0300

    feat: removeImpares and inverte created

--- lista1/changeNums.py
+++ lista1/changeNums.py
@@ -6,4 +6,19 @@ def zeraPares(n):
     else:
         return zeraPares(n//10)*10 + n%10
 
+def removeImpares(n):
+    if n < 10:
+        return 0 if n % 2 == 1 else n
+    if n % 2 == 1:
+        return removeImpares(n//10)
+    else:
+        return removeImpares(n//10)*10 + n%10
+
+def inverte(n, i=0):
+    if n < 10:
+        return i*10+n
+    return inverte(n//10, i*10 + n%10)
+
 print(zeraPares(1234))
+print(removeImpares(1234))
+print(inverte(1234))

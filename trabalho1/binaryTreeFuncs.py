82f2188840e9606adc94155b6a6bf8290529a970
Lucas Rocha <lr359508@gmail.com>
2024-04-10 12:19:50 -0300

    feat: searchN created

--- trabalho1/binaryTreeFuncs.py
+++ trabalho1/binaryTreeFuncs.py
@@ -2,12 +2,12 @@
 from node import Node
 from util import Queue
 
-def nodeCount(root : Node):
+def nodeCount(root):
     if not root:
         return 0
     return nodeCount(root.left) + nodeCount(root.right) + 1
 
-def searchMax(root : Node):
+def searchMax(root):
     q = Queue(nodeCount(root))
     q.enqueue(root)
     max_value = root.data
@@ -22,6 +22,12 @@ def searchMax(root : Node):
 
     return max_value
 
+def searchN(root, n):
+    if not root:
+        return False
+    
+    return root.data == n or searchN(root.left, n) or searchN(root.right, n)
+
 n = Node(2)
 n.left = Node(-3)
 n.right = Node(7)
@@ -29,3 +35,5 @@ n.left.left = Node(5)
 
 print(nodeCount(n))
 print(searchMax(n))
+print(searchN(n, 7))
+print(searchN(n, 0))

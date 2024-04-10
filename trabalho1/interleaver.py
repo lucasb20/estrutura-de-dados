06f54205fb9c8fabd6cc93b9255e0512e826441f
Lucas Rocha <lr359508@gmail.com>
2024-04-10 11:51:35 -0300

    feat: interleaver created

---
+++ trabalho1/interleaver.py
@@ -0,0 +1,26 @@
+
+from util import Stack, Queue
+
+
+def interleaver(s):
+    q = Queue(s.count*2)
+
+    count = s.count // 2
+
+    for _ in range(count): q.enqueue(s.pop())
+
+    while not s.isEmpty():
+        q.enqueue(q.dequeue())
+        q.enqueue(s.pop())
+
+    return q
+
+
+stack = Stack()
+stack.push(5)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+
+print(stack)
+print(interleaver(stack))

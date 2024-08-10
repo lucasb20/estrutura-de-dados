e628c4171b1ea6f2adf30e382a42a319d4f19905
Lucas Rocha <lr359508@gmail.com>
2024-08-10 12:16:31 -0300

    feat: adição de impressão de árvore

--- lista5/AVLTree.py
+++ lista5/AVLTree.py
@@ -1,13 +1,16 @@
 
 class Node():
     def __init__(self, key = None):
-        self.key = None
+        self.key = key
         self.parent = None
         self.left = None
         self.right = None
 
+    def __str__(self):
+        return f'Node(key={self.key})'
+
 class AVLTree():
-    def __init__(self, key : Node = None):
+    def __init__(self, key = None):
         self.root = Node(key)
 
     def insert(self, key):
@@ -22,10 +25,19 @@ class AVLTree():
             elif key < node.key:
                 if not node.left:
                     node.left = Node(key)
+                    node.left.parent = node
                     return
                 node = node.left
             else:
                 if not node.right:
                     node.right = Node(key)
+                    node.right.parent = node
                     return
                 node = node.right
+
+def printTree(root : Node):
+    if not root:
+        return
+    printTree(root.left)
+    print(root)
+    printTree(root.right)
